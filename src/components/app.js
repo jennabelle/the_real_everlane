b6d03f8056a9479a3ebf74489dc2f1509bc8d64d
Jenna Vuong <jennavuong@gmail.com>
2016-07-28 14:41:55 -0700

    css modifications

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,7 @@ import { Link } from 'react-router'
 export default class App extends Component {
   render() {
     return (
-      <div>
-      	React simple starter
-      	<img src='http://www.triborodesign.com/public/user-content/files/2014/03/21/everlane03-1309.jpg' />
+      <div className='app_background'>
       </div>
     );
   }
