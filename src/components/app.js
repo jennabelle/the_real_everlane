20d87768ba0cfc706fb00c2299acc903df3e3de6
Jenna Vuong <jennavuong@gmail.com>
2016-07-28 20:02:25 -0700

    change back use webpack-dev-server for now, background and input search field coexist

--- src/components/app.js
+++ src/components/app.js
@@ -2,14 +2,33 @@ import React, { Component } from 'react'
 import { Link } from 'react-router'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AutoComplete from 'material-ui/AutoComplete'
+// import SearchBar from './searchBar'
+
+const colors = [
+  'Red',
+  'Orange',
+  'Yellow',
+  'Green',
+  'Blue',
+  'Purple',
+  'Black',
+  'White',
+]
 
 export default class App extends Component {
   render() {
     return ( 
-      <MuiThemeProvider>
       	<div className='app_background'>
+      		<MuiThemeProvider>
+      			<AutoComplete
+      				className='searchBar'
+      				fullWidth={ true }
+			      	floatingLabelText="Search products worn by real people..."
+			      	filter={AutoComplete.caseInsensitiveFilter}
+			      	dataSource={colors}
+			    />
+      		</MuiThemeProvider>
       	</div>
-      </MuiThemeProvider>
     );
   }
 }
