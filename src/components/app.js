62e45192edcd6cadb031f110c9cba485c08bc012
Jenna Vuong <jennavuong@gmail.com>
2016-07-29 12:44:02 -0700

    add functionality when user presses enter

--- src/components/app.js
+++ src/components/app.js
@@ -2,20 +2,9 @@ import React, { Component } from 'react'
 import { Link } from 'react-router'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AutoComplete from 'material-ui/AutoComplete'
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
 injectTapEventPlugin();
-// import SearchBar from './searchBar'
-
-const terms = [
-	'Bags',
-	'Blouse',
-  	'Dress',
-  	'Outerwear',
-  	'Pants',
-  	'Shoes',
-  	'T-Shirt'
-]
 
 export default class App extends Component {
 
@@ -25,23 +14,30 @@ export default class App extends Component {
 		this.state = { dataSource: [] }
 
 		this.handleUpdateInput = this.handleUpdateInput.bind(this)
+		this.onKeyDown = this.onKeyDown.bind(this)
 	}
- 	handleUpdateInput(value) {
-
-		console.log('value: ', value)
+ 	handleUpdateInput(value) { // save user input
+		console.log('this.state.dataSource: ', this.state.dataSource)
 
 		this.setState({
 			dataSource: [ value ]
-		});
-	};
+		})
+	}
+	onKeyDown(value) { // when user clicks 'enter'
+
+		if (value.keyCode === 13) {
+			console.log('user pressed enter!')
+		}
+	}
 	render() {
-		return ( 
+		return (
       	<div className='app_background'>
       		<MuiThemeProvider>
       			<AutoComplete
 			          hintText="Type anything"
-			          dataSource={this.state.dataSource}
-			          onUpdateInput={this.handleUpdateInput}
+			          dataSource={ this.state.dataSource }
+			          onUpdateInput={ this.handleUpdateInput }
+			          onKeyDown={ this.onKeyDown }
 			          floatingLabelText="Search for products worn by real people" 
 			          fullWidth={ true }
 			          />
