b4d468c2afc05617a1e01113e543302bb8417b68
Jenna Vuong <jennavuong@gmail.com>
2016-07-29 13:42:52 -0700

    add axios

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import axios from 'axios'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AutoComplete from 'material-ui/AutoComplete'
 import injectTapEventPlugin from 'react-tap-event-plugin'
