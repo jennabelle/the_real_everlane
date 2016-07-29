0ce38d769c50ff695fd36da7c93e3004a86ad719
Jenna Vuong <jennavuong@gmail.com>
2016-07-29 13:22:47 -0700

    add owner id to network helper class

--- src/helpers/networkHelper.js
+++ src/helpers/networkHelper.js
@@ -1,13 +1,17 @@
 const url = 'https://api.particle.io/v1/devices'
-const access_token = 'd6576383889e1526c95853391923584b508071c4'
+const access_token = '15787539-ETzhXMENTMOT6g5UD4VK8W2sxHMVMTfTXi5t5z6EL'
+const owner_id = 15787539
 
 // can also pass as props too
 export default {
-  
+	
   getUrl() {
     return url
   },
   getAccessToken() {
     return access_token
+  },
+  getOwnerId() {
+  	return owner_id
   }
 }
