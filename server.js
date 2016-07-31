4add19ac37828f045d110a1b45ec1f8d469b1d58
Jenna Vuong <jennavuong@gmail.com>
2016-07-30 19:28:30 -0700

    got app to work correctly, added .env

--- server.js
+++ server.js
@@ -1,20 +1,48 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-
+var Twitter = require('twitter');
 var app = express();
+var PORT = process.env.PORT || 3000;
+var DotEnv = require('dotenv');
 
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false, limit: '500mb' })); // jenna
-app.use(bodyParser.json({ limit: '500mb' })); // jenna
+DotEnv.config();
 
-// serve static files like index.html, css etc.
+// import express from 'express'
+// import bodyParser from 'body-parser'
+// import path from 'path'
+// import Twitter from 'twitter'
+
+var client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+const api_url = 'https://api.twitter.com/1.1/search/tweets.json';
+
+// configure express to use body-parser as middleware
+app.use(bodyParser.urlencoded({ extended: false, limit: '500mb' }));
+app.use(bodyParser.json({ limit: '500mb' }));
+
+// serve static files like index.html, css etc
 app.use( express.static( __dirname ) );
 
-var PORT = process.env.PORT || 3000;
+app.post('/api/getTweets', function(req, res) {
+
+	var params = { q: req.body.searchQuery + '+everlane', filter: 'media' };
+
+	client.get(api_url, params, function(error, tweets, resp) {
+
+		if (error) { console.log('error: ', error) }
+
+		if (!error) {
+			res.send(tweets);
+		}
+	});
+});
 
 var server = app.listen(PORT, function() {
-
   console.log('\nProduction Express server running at localhost:' + PORT);
-
 });
--- src/components/app.js
+++ src/components/app.js
@@ -4,45 +4,70 @@ import axios from 'axios'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AutoComplete from 'material-ui/AutoComplete'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import NetworkHelperClass from '../helpers/networkHelper'
+import Tweet from 'react-tweet'
 
-injectTapEventPlugin();
+injectTapEventPlugin()
 
 export default class App extends Component {
 
 	constructor(props) {
 		super(props)
 
-		this.state = { dataSource: [] }
+		this.state = { dataSource: [], tweets: [] }
 
 		this.handleUpdateInput = this.handleUpdateInput.bind(this)
 		this.onKeyDown = this.onKeyDown.bind(this)
 	}
  	handleUpdateInput(value) { // save user input
-		console.log('this.state.dataSource: ', this.state.dataSource)
 
 		this.setState({
-			dataSource: [ value ]
+			dataSource: [ value ],
+			tweets: []
 		})
 	}
-	onKeyDown(value) { // when user clicks 'enter'
+	onKeyDown(value) {
 
-		if (value.keyCode === 13) {
-			console.log('user pressed enter!')
+		if ( value.keyCode === 13 ) { // if user clicks 'enter'
+
+	  		console.log('this.state.dataSource: ', this.state.dataSource)
+
+	  		axios.post('/api/getTweets', { searchQuery: this.state.dataSource.join() })
+	  		.then( (resp) => {
+	  			console.log('axios post response: ', resp.data.statuses)
+
+	  			this.setState({ tweets: resp.data.statuses });
+	  		})
+	  		.catch( (resp) => {
+	  			console.log('axios catch response: ', resp)
+	  		})
 		}
 	}
 	render() {
 		return (
       	<div className='app_background'>
       		<MuiThemeProvider>
-      			<AutoComplete
-			          hintText="Type anything"
-			          dataSource={ this.state.dataSource }
-			          onUpdateInput={ this.handleUpdateInput }
-			          onKeyDown={ this.onKeyDown }
-			          floatingLabelText="Search for products worn by real people" 
-			          fullWidth={ true }
-			          />
+      			<div>
+	      			<AutoComplete
+	      				className='searchBar'
+				         hintText='Type anything'
+				         dataSource={ this.state.dataSource }
+				         onUpdateInput={ this.handleUpdateInput }
+				         onKeyDown={ this.onKeyDown }
+				         floatingLabelText='Search for products worn by real people'
+				         fullWidth={ true }
+				         />
+
+				      <div className='view_tweets'>
+				         { this.state.tweets.map(function(name, index) {
+
+								return (
+									<div key={ index }>
+										<Tweet data={ name } />
+									</div>
+								)}
+							)}
+				      </div>
+			      </div>
       		</MuiThemeProvider>
       	</div>
     );
