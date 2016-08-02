9b2bd0eb8662d6366576edec664550dca2acfea9
Jenna Vuong <jennavuong@gmail.com>
2016-08-01 19:15:27 -0700

    add test specs

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -11,4 +11,26 @@ describe('App' , () => {
   it('renders something', () => {
     expect(component).to.exist;
   });
+
+  it('has has the correct class', () => {
+  	expect(component).to.have.class('app_background');
+  });
+
+  it('has a input text area', () => {
+  	// expect(component.find('textarea')).to.exist;
+  })
+
+  it('shows the correct text', () => {
+  	// expect(component).to.contain('React simple starter');
+  });
+
+  it('pressing enter displays tweets', () => {
+  	// simulate enter
+  	// shows a tweet in specific styled way
+  });
+
+  it('given a list of tweets, it shows all the tweets', () => {
+
+  });
+
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -5,27 +5,28 @@ import TestUtils from 'react-addons-test-utils';
 import jsdom from 'jsdom';
 import chai, { expect } from 'chai';
 import chaiJquery from 'chai-jquery';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from '../src/reducers';
+import App from '../src/components/app';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import reducers from '../src/reducers';
 
+// set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 global.navigator = global.window.navigator;
 const $ = _$(window);
 
+// set up chai-jquery
 chaiJquery(chai, chai.util, $);
 
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
-      <ComponentClass {...props} />
-    </Provider>
-  );
+// build 'renderComponent' helper that should render a given react class
+function renderComponent(ComponentClass) {
+  const componentInstance =  TestUtils.renderIntoDocument(ComponentClass);
 
   return $(ReactDOM.findDOMNode(componentInstance));
 }
 
+// build helper for simulating events
 $.fn.simulate = function(eventName, value) {
   if (value) {
     this.val(value);
@@ -33,4 +34,5 @@ $.fn.simulate = function(eventName, value) {
   TestUtils.Simulate[eventName](this[0]);
 };
 
+// export functions
 export {renderComponent, expect};
