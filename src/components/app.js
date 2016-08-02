ac0f52f7fea9197970b26c813160475e9718d4c5
Jenna Vuong <jennavuong@gmail.com>
2016-08-02 00:15:44 -0700

    remove unnecessary comments

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import SearchBar from './searchBar'
-// import AutoComplete from 'material-ui/AutoComplete'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-// import Tweet from 'react-tweet'
 
 injectTapEventPlugin()
 
@@ -12,36 +10,7 @@ export default class App extends Component {
 
 	constructor(props) {
 		super(props)
-
-		// this.state = { dataSource: [], tweets: [] }
-
-		// this.handleUpdateInput = this.handleUpdateInput.bind(this)
-		// this.onKeyDown = this.onKeyDown.bind(this)
 	}
- // 	handleUpdateInput(value) { // save user input
-
-	// 	this.setState({
-	// 		dataSource: [ value ],
-	// 		tweets: []
-	// 	})
-	// }
-	// onKeyDown(value) {
-
-	// 	if ( value.keyCode === 13 ) { // if user clicks 'enter'
-
-	//   		console.log('this.state.dataSource: ', this.state.dataSource)
-
-	//   		axios.post('/api/getTweets', { searchQuery: this.state.dataSource.join() })
-	//   		.then( (resp) => {
-	//   			console.log('axios post response: ', resp.data.statuses)
-
-	//   			this.setState({ tweets: resp.data.statuses });
-	//   		})
-	//   		.catch( (resp) => {
-	//   			console.log('axios catch response: ', resp)
-	//   		})
-	// 	}
-	// }
 	render() {
 		return (
       	<div className='app_background'>
