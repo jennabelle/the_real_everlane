0d590906ee174fb716fb832a602d28723003d4c6
Jenna Vuong <jennavuong@gmail.com>
2016-08-02 01:46:57 -0700

    add test spec for searchResults

---
+++ test/components/searchResults_test.js
@@ -0,0 +1,19 @@
+import { renderComponent , expect } from '../test_helper'
+import React from 'react'
+import SearchResults from '../../src/components/searchResults'
+
+describe('SearchResults' , () => {
+  let component
+
+  beforeEach(() => {
+    component = renderComponent( React.createElement(SearchResults) )
+  })
+
+  it('renders something', () => {
+    expect(component).to.exist
+  })
+
+  it('has the correct class', () => {
+  	expect(component).to.have.class('view_tweets')
+  })
+})
