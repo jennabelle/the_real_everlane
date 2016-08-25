125bcc897e5e30fcb21da136d05797e932321226
Jenna Vuong <jennavuong@gmail.com>
2016-08-24 20:56:56 -0700

    improved ux, if no search results, no matches text is rendered

--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -7,16 +7,20 @@ export default class SearchResults extends Component {
 		super(props)
 	}
 	render() {
+
+		console.log('this.props.tweets.length: ', this.props.tweets.length)
 		return (
 			<div className='view_tweets'>
-				{ this.props.tweets.map(function(name, index) {
+				{ 
+					(this.props.tweets.length > 0) ? 
+						this.props.tweets.map(function(name, index) {
 
-					return (
-						<div key={ index }>
-							<Tweet data={ name } />
-						</div>
-					)}
-				)}
+							return (
+								<div key={ index }>
+									<Tweet data={ name } />
+								</div>
+							)} ) : <p>There are no matches.</p>
+				}
 			</div>
    		);
    }
