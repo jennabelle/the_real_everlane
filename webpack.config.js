d5b08507c0a329f1ef49ad61da6a044db14a5e2f
Jenna Vuong <jennavuong@gmail.com>
2016-09-11 16:03:23 -0700

    get rid of babel-loader, causing heroku deploy errors

--- webpack.config.js
+++ webpack.config.js
@@ -11,14 +11,6 @@ module.exports = {
   },
   module: {
     loaders: [{
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-    },
-    {
       exclude: /node_modules/,
       loader: 'babel',
       query: {
